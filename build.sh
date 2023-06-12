a5bd27a45e08ca9f2085b7b8c9683e3e13c4a94d
Saptarshi Ghosh <121953250+RishiCSE23@users.noreply.github.com>
2023-06-12 16:24:04 +0100

    fixes built.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -e  # breaks the script if encounters an exception
 sudo apt -y update # update the repo list 
 sudo apt -y install python3 python3-pip python3-venv # install python 
 python3 -m venv venv  # create a venv
-source venv/bin.activate  # activate the venv 
+source venv/bin/activate  # activate the venv 
 python3 -m pip install --upgrade pip # upgrade pip to its latest version
 pip install -r requirements.txt # install dependencies 
 
